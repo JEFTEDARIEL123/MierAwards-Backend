80ac8133e6b3f210b5125e5ae12ecb68f43c074b
JEFTEDARIEL123 <jeftedariel3@gmail.com>
2023-08-31 12:24:27 -0600

    asas

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ const colors = require('colors');
 
 app.use(bodyParser.json());
 app.use(cors());
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://tu-frontend.com'); // Reemplaza con la URL de tu frontend
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 const version = "1.0 Release"
 
 mongoose.connect(Mongodb, {
