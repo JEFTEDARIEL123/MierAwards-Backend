f3368f3696b25e1bd9845df42c0ffc42a1a67a87
JEFTEDARIEL123 <jeftedariel3@gmail.com>
2023-09-05 20:14:20 -0600

    final change

--- index.js
+++ index.js
@@ -2,23 +2,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const ADDR = process.env.ADDRESS;
 const Mongodb = process.env.MONGODB
-const cors = require('cors');
 const colors = require('colors');
 
 app.use(bodyParser.json());
-app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://tu-frontend.com'); // Reemplaza con la URL de tu frontend
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-const version = "1.0 Release"
+
+app.use(
+  cors()
+);
+
+const version = "1.0.1 Release"
 
 mongoose.connect(Mongodb, {
   useNewUrlParser: true,
