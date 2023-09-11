088a11e61162712781bd86068dd447d5c731594b
Jeft <63319744+JEFTEDARIEL123@users.noreply.github.com>
2023-09-10 18:05:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.get('/api/form/:userNickname', (req, res) => {
 });
 
 app.post('/api/form', (req, res) => {
-  const newForm = new Form({ frescoleche, apodo, glowup, racista, pareja, divertido, aesthetic, profesor, fiestero, inteligente, momentoaño, ausente, grupito, atletico, pipi, empayazado, ship, fiel, infiel, lagarto, toxico, chismoso, asaltasilos, desvelado, abuelo, artista, hater, duo, nickname }) = req.body;
+  const { frescoleche, apodo, glowup, racista, pareja, divertido, aesthetic, profesor, fiestero, inteligente, momentoaño, ausente, grupito, atletico, pipi, empayazado, ship, fiel, infiel, lagarto, toxico, chismoso, asaltasilos, desvelado, abuelo, artista, hater, duo, nickname } = req.body;
 
   Form.findOne({ nickname })
     .then((existingForm) => {
